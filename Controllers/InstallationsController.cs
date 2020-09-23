e54d067b9a9cf6f4c66e6a0b1fd79fef74c3e45e
garb0nzo <54480601+weetardeded@users.noreply.github.com>
2020-09-23 19:09:19 +0200

    init

---
+++ Controllers/InstallationsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SCDBackend.DataAccess;
+using SCDBackend.Models;
+using System.Text.Json;
+
+namespace SCDBackend.Controllers
+{
+    [Route("api/installations")]
+    [ApiController]
+    public class InstallationsController : ControllerBase
+    {
+        [HttpGet("all")]
+        public async Task<IActionResult> getAllInstallations()
+        {
+            CosmosConnector cc = CosmosConnector.instance;
+            await cc.establishConnection(); // ensure that we are connected to the db before use
+
+            List<Installation> installations = await cc.GetInstallationsAsync();
+
+            string json = JsonSerializer.Serialize(installations);
+
+            return Ok(json);
+        }
+    }
+}
---
+++ DataAccess/CosmosConnector.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using SCDBackend.Models;
+
+namespace SCDBackend.DataAccess
+{
+    public sealed class CosmosConnector
+    {
+
+        public static CosmosConnector instance { get; } = new CosmosConnector();
+
+        private static readonly string EndpointUri = "https://fungler.documents.azure.com:443/";
+        private static readonly string PrimaryKey = "Gs9XLbKvsstuGzfUpbCYNufDBER0o9Ony3WmBo8drTMp4ugd48s2xqAiKQI5Ve9yXiBFnqXqu3Sj8T607uouPA==";
+
+        private static string databaseId = "frontend_test";
+        private static string containerId = "dummyInstallations";
+
+        private static CosmosClient cosmosClient;
+        private static Database database;
+        private static Container container;
+
+        public CosmosConnector()
+        {
+            Console.WriteLine("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        }
+
+
+        private static async Task InitAsync()
+        {
+            if (cosmosClient == null || database == null || container == null)
+            {
+                cosmosClient = new CosmosClient(EndpointUri, PrimaryKey);
+                database = await cosmosClient.CreateDatabaseIfNotExistsAsync(databaseId);
+                container = await database.CreateContainerIfNotExistsAsync(new ContainerProperties(containerId, "/installation"));
+            }
+        }
+
+        public async Task<List<Installation>> GetInstallationsAsync()
+        {
+            QueryDefinition qd = new QueryDefinition("SELECT * FROM c");
+
+            FeedIterator<Installation> queryResultSetIterator = container.GetItemQueryIterator<Installation>(qd);
+            List<Installation> res = new List<Installation>();
+
+            while (queryResultSetIterator.HasMoreResults)
+            {
+                FeedResponse<Installation> currentResultSet = await queryResultSetIterator.ReadNextAsync();
+
+                foreach (Installation installation in currentResultSet)
+                {
+                    res.Add(installation);
+                }
+            }
+
+            return res;
+        }
+
+        public async Task establishConnection()
+        {
+            try
+            {
+                await InitAsync();
+            }
+            catch (CosmosException e)
+            {
+                Console.WriteLine("Cosmos except");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Except");
+            }
+        }
+    }
+}
