67e65e442ad27fc344b3fc5efe5472dd0b47d9a7
Annika Ehlers <e_hlers@live.dk>
2020-10-20 21:19:37 +0200

    Subscription

--- DataAccess/CosmosConnector.cs
+++ DataAccess/CosmosConnector.cs
@@ -103,6 +103,7 @@ namespace SCDBackend.DataAccess
         // overload for installation copy
         public async Task CreateInstallationAsync(InstallationCopy installation)
         {
+            
             await EstablishConnection();
             Container c = cosmosClient.GetDatabase(databaseId).GetContainer(containerId);
             var installationItemResponse = await c.CreateItemAsync<InstallationCopy>(installation, new PartitionKey(installation.installation));
