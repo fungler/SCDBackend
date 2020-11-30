30f6d8c4ddc6786d2b7c069401268120fcf17e91
Annika Ehlers <e_hlers@live.dk>
2020-11-30 12:30:42 +0100

    Finish setup of pipeline

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -18,11 +18,5 @@ namespace UnitTest
         {
             Assert.True(true);
         }
-
-        [Fact]
-        public void Test2()
-        {
-            Assert.True(false);
-        }
     }
 }
