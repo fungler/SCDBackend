39524e425a3720ba37548b9244eff9e6cbb9723f
Annika Ehlers <e_hlers@live.dk>
2020-12-16 20:39:20 +0100

    Add IntegrationTests Project and change name for Unit Test Project

---
+++ tests/SCDBackend.UnitTests/TestClasses/UnitTest.cs
@@ -0,0 +1,19 @@
+using SCDBackend.Models;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace UnitTest
+{
+    public class UnitTest
+    {
+
+        [Fact]
+        public void pass()
+        {
+            Assert.True(true);
+        }
+
+    }
+}
